54c316e96e427b9b5d4873c80406d7609750c4b8
MD. SHAKHAWAT HOSSAIN <142770640+dev-shakhawat@users.noreply.github.com>
2026-07-15 07:40:15 +0600

    Add ProductBanner component with image viewer

--- index.tsx
+++ index.tsx
@@ -1,1 +1,19 @@
 
+import productImage from "@/public/images/productimage.jpg";
+import ImageZoomViewer from "./product-image-viewer"; 
+
+export default function ProductBanner({}: {}) {
+  const productInfo = {
+    name: "Pebble Smartwatch",
+    image: productImage,
+    price: 199.99,
+    isInStock: true,
+  };
+
+  return (
+       <ImageZoomViewer
+        imageSrc={productInfo.image.src}
+        className="w-full"
+      />
+  );
+}
